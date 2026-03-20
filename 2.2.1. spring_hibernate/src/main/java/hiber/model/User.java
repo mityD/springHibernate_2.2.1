c7e8743896f33df14712e477f04eb97d2dec17e3
mityD <DKulikov123@yandex.ru>
2026-03-20 16:11:31 +0300

    2.2.1_ready

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,6 +19,16 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade= CascadeType.ALL)
+   private Car car;
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
+   }
+
    public User() {}
    
    public User(String firstName, String lastName, String email) {
@@ -57,5 +67,15 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
    }
 }
