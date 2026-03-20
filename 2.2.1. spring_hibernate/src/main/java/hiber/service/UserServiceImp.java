6669f0cff677e76522d0b1815561c6cfaf390de3
mityD <DKulikov123@yandex.ru>
2026-03-20 22:24:10 +0300

    2.2.1_ready2

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class UserServiceImp implements UserService {
 
     @Autowired
@@ -20,13 +21,11 @@ public class UserServiceImp implements UserService {
         userDao.add(user);
     }
 
-    @Transactional(readOnly = true)
     @Override
     public List<User> listUsers() {
         return userDao.listUsers();
     }
 
-    @Transactional(readOnly = true)
     @Override
     public User getUserByModelAndSeries(String model, int series) {
         return userDao.getUserByModelAndSeries(model, series);
